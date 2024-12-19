8a59b35b5a8c90c3e4399e361fb6ba6152b14f39
Fernando Sanchez <perracolabs@gmail.com>
2024-12-19 15:02:26 +0100

    Migrated to dokka 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ mavenPublishing {
 
     configure(
         KotlinJvm(
-            javadocJar = JavadocJar.Dokka("dokkaHtml"),
+            javadocJar = JavadocJar.Dokka("dokkaGeneratePublicationHtml"),
             sourcesJar = true,
         )
     )
